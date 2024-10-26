6aa8bbd78cb72a8a83ef670b478636ee6dc18496
ClaraGard <clara.gard94@gmail.com>
2024-10-26 19:00:05 +0200

    Removed old backend and node_modules

--- backend/migrations/20241025100742-add-userid-index-to-tasklists.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addIndex('TaskLists', ['userId']);
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('TaskLists', ['userId']);
-  }
-};
--- backend/migrations/20241025100751-add-tasklistid-index-to-tasks.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addIndex('Tasks', ['taskListId']);
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('Tasks', ['taskListId']);
-  }
-};
