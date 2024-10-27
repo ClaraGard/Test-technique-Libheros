de5847998fcc76054d0980f5c105d8ec29c493e7
ClaraGard <clara.gard94@gmail.com>
2024-10-27 16:43:56 +0100

    Update index.js
    Compatibility with React 18+

--- libheros-app/src/index.js
+++ libheros-app/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use this import for createRoot
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement); // Initialize the root
+root.render(<App />); // Render the app using createRoot
